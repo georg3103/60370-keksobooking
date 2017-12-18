b780b7d80f3ff3e7e2e4a245de9b25b0c654bf72
George Mkrtchyan <gnmkrtchyan@gmail.com>
2017-12-18 21:10:22 +0300

    работает во всех браузерах

--- js/show-card.js
+++ js/show-card.js
@@ -5,16 +5,11 @@
 
     var MAP_PINS_CLASS = '.map__pins';
     var MAP_PIN_CLASS = 'map__pin';
-    var MAP_PIN_MAIN_CLASS = 'map__pin--main';
 
     pins.addEventListener('click', function (e) {
-      var target = e.target;
-      if (target.parentNode.classList.contains(MAP_PIN_MAIN_CLASS)) {
-        return;
-      } else if (target.parentNode.classList.contains(MAP_PIN_CLASS)) {
-        target = target.parentNode;
-        processPin(target);
-      }
+      var currentPin = e.target.closest('.' + MAP_PIN_CLASS);
+
+      processPin(currentPin);
     });
 
     pins.addEventListener('keydown', function (e) {
